72665e71b6bb3b12eac6c682389842a780a66676
Ardra M <ardra.m@vonnue.com>
2026-06-22 09:41:42 +0530

    feat: log typeof of primitive types

---
+++ DAY-1/types.js
@@ -0,0 +1,14 @@
+console.log(typeof([]+{}))
+console.log(typeof('3'-1))
+console.log(typeof(0==false))
+console.log(typeof(1234n))
+console.log(typeof(Symbol()))
+let y=null
+console.log(y)
+console.log(typeof(x))
+console.log(typeof function (){})
+console.log(typeof(null))
+
+
+
+
