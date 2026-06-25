e61beff222e490432b2e6f92e99aa37a6263b1ca
Ardra M <ardra.m@vonnue.com>
2026-06-25 09:31:45 +0530

    fix: fix formatting issues

--- DAY-1/scope.js
+++ DAY-1/scope.js
@@ -1,5 +1,4 @@
 //scope of var, let, and const//
-
 if(true){
          var x=10;
      }
@@ -12,61 +11,44 @@ if(true){
      const z=20;
  }
  console.log(z)
-
-//----------------//
 //Re-declaring variables with var and const//
-
- var x=10;
- var x=20;
- console.log(x)
-
- const y=10;
- const y=20;
- console.log(y)
-
+var x=10;
+var x=20;
+console.log(x)
+const y=10;
+const y=20;
+console.log(y)
 //updating variables with var and const//
-
- var y=10;
- y=20;
- console.log(y);
-
- const z=10;
- z=20;
- console.log(z);
-
+var y=10;
+y=20;
+console.log(y);
+const z=10;
+z=20;
+console.log(z);
 //hoisting behaviour of var and let//
-
- console.log(x);
- var x=10;
- console.log(x);
-
- console.log(x);
- let x=10;
- console.log(x);
-
+console.log(x);
+var x=10;
+console.log(x);
+console.log(x);
+let x=10;
+console.log(x);
 //var and let with for loop//
-
 for(var i=0;i<3;i++){
     console.log(i)
 }
 console.log(i)
-
 for(let i=0;i<3;i++){
     console.log(i)
 }
 console.log(i)
-
 //temporal dead zone// 
-
 console.log(a)//throws ReferenceError
 let a=10 
 console.log(a)//undefined
 var a=10
-
 //nested functions //
 function One(){
     var x=10;
-    
     function Two(){
         let y=20;
         function Three(){
@@ -80,9 +62,7 @@ function One(){
     Two();
 }
 One();
-
 //var-in-loop closure bug//
-
 for(var i=0;i<4;i++){
     setTimeout(() => {console.log(i);},1000);
 }
